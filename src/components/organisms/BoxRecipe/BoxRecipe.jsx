f2d3f2bdbb09310e8724ec135060c7a32e93f987
guilher-phellippe <guilhermephellippe@outlook.com>
2023-04-14 14:36:32 -0300

    resize image main

--- src/components/organisms/BoxRecipe/BoxRecipe.jsx
+++ src/components/organisms/BoxRecipe/BoxRecipe.jsx
@@ -10,7 +10,7 @@ export const BoxRecipe = ({ recipe, area, index }) => {
         <Link className={`rounded-lg relative cursor-pointer flex flex-col justify-center items-center overflow-hidden ${area} group`} to={`/recipe/${id}`}>
             <Img
                 className="absolute top-0 left-0 w-full h-full object-cover opacity-50 z-0 transition-opacity duration-300 ease-linear group-hover:opacity-80"
-                src={images_recipe[0].medium}
+                src={index === 0 ? images_recipe[0].medium : images_recipe[0].small}
                 alt={`imagem de ${name_recipe}`} />
             <Info3MostViewedRecipes recipe={recipe} index={index}/>
             <HoverInfo3MostViewedRecipes recipe={recipe} />
