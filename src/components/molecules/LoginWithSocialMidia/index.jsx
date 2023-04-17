343f97616c73363f61c5623a0d14593b04fee330
guilher-phellippe <guilhermephellippe@outlook.com>
2023-04-17 20:47:35 -0300

    add login with google and facebook

--- src/components/molecules/LoginWithSocialMidia/index.jsx
+++ src/components/molecules/LoginWithSocialMidia/index.jsx
@@ -1,42 +1,91 @@
-// import GoogleLogin from 'react-google-login';
-// import GoogleLogout from 'react-google-login';
+import { initializeApp } from 'firebase/app'
+import { FacebookAuthProvider, getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth"
 
-// const clientId = '742227891217-44e1lhaikhg2u3a4kjeeahroq8938ct2.apps.googleusercontent.com';
+import { FcGoogle } from "react-icons/fc"
+
+import { Button } from "../../atoms/Button"
+import { useNotificationApi, useUserApi } from '../../../hooks/useApi';
+import { useNavigate } from 'react-router-dom';
+import { MdFacebook } from 'react-icons/md';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDkpJkYLEFE3r-oyqpdG_4uGJEo9IDYAo8",
+    authDomain: "tem-sabor-auth.firebaseapp.com",
+    projectId: "tem-sabor-auth",
+    storageBucket: "tem-sabor-auth.appspot.com",
+    messagingSenderId: "452576142508",
+    appId: "1:452576142508:web:144b4553e640ed2f68ab09"
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app)
+const provider = new GoogleAuthProvider();
+const providerFb = new FacebookAuthProvider();
 
 export const LoginWithSocialMidia = () => {
+    const notificationApi = useNotificationApi()
+    const userApi = useUserApi();
+    const navigate = useNavigate();
 
-    // const onSuccess = (response) => {
-    //     console.log(response);
-    // };
 
-    // const onFailure = (response) => {
-    //     console.log(response);
-    // };
+    const handleGoogleLogin = async () => {
+        signInWithPopup(auth, provider)
+            .then(async (result) => {
+               const userData = result.user.providerData[0];
 
-    // const logout = () => {
-    //     console.log('Usuário deslogado!')
-    // };
+                const user = {
+                    id: userData.uid,
+                    name: userData.displayName,
+                    email: userData.email,
+                    photo: userData.photoURL,
+                    password: userData.displayName.split(' ').join(''),
+                }
 
+                const response = await userApi.createNewUser(user);
+                if (!response.error) {
+                    console.log(response)
+                    notificationApi.newNotificationAlreadyExist("e7682967-ea1e-4b46-8d2c-d1621dac5dd1", response.id)
+                    localStorage.setItem('token', JSON.stringify(response))
+                    navigate('/')
+                } else {
+                    const { data } = await userApi.authenticateUser(
+                        {
+                            email: userData.email.toLowerCase(),
+                            password: userData.displayName.split(' ').join('')
+                        }
+                    );
+
+                    if (data) {
+                        localStorage.setItem("token", JSON.stringify(data));
+                        navigate('/')
+                    } else alert("Usuario ou senha incorretos!")
+                }
+            })
+            .catch((error) => {
+                console.error(error)
+            });
+    }
+
+    const handleFacebookLogin = async () => {
+        signInWithPopup(auth, providerFb)
+            .then(async (result) => {
+               console.log(result)
+            })
+            .catch((error) => {
+                console.error(error)
+            });
+    }
 
     return (
         <>
-            <div id="buttonDiv">
-                oi
-                {/* <>
-                    <GoogleLogin
-                        clientId={clientId}
-                        buttonText="Faça login com o Google"
-                        onSuccess={onSuccess}
-                        onFailure={onFailure}
-                        cookiePolicy={'single_host_origin'}
-                    />
-                    <GoogleLogout
-                        clientId={clientId}
-                        buttonText="Logout"
-                        onLogoutSuccess={logout}
-                    />
-                </> */}
-            </div>
+            <Button event={handleGoogleLogin} customClass='border rounded-3xl flex items-center gap-3 text-s1_5 px-8 py-4'>
+                <FcGoogle />
+                Entrar com google
+            </Button>
+            <Button event={handleFacebookLogin} customClass='border rounded-3xl flex items-center gap-3 text-s1_5 px-8 py-4'>
+                <MdFacebook className='fill-blue-700' />
+                Entrar com Facebook
+            </Button>
         </>
     )
 }
