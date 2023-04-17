2b4d8f79ec57bbe7428df0d462002885c4036605
guilher-phellippe <guilhermephellippe@outlook.com>
2023-04-17 14:19:37 -0300

    uninstall google-login

--- src/components/molecules/LoginWithSocialMidia/index.jsx
+++ src/components/molecules/LoginWithSocialMidia/index.jsx
@@ -1,27 +1,28 @@
-import GoogleLogin from 'react-google-login';
-import GoogleLogout from 'react-google-login';
+// import GoogleLogin from 'react-google-login';
+// import GoogleLogout from 'react-google-login';
 
-const clientId = '742227891217-44e1lhaikhg2u3a4kjeeahroq8938ct2.apps.googleusercontent.com';
+// const clientId = '742227891217-44e1lhaikhg2u3a4kjeeahroq8938ct2.apps.googleusercontent.com';
 
 export const LoginWithSocialMidia = () => {
 
-    const onSuccess = (response) => {
-        console.log(response);
-    };
+    // const onSuccess = (response) => {
+    //     console.log(response);
+    // };
 
-    const onFailure = (response) => {
-        console.log(response);
-    };
+    // const onFailure = (response) => {
+    //     console.log(response);
+    // };
 
-    const logout = () => {
-        console.log('Usuário deslogado!')
-    };
+    // const logout = () => {
+    //     console.log('Usuário deslogado!')
+    // };
 
 
     return (
         <>
             <div id="buttonDiv">
-                <>
+                oi
+                {/* <>
                     <GoogleLogin
                         clientId={clientId}
                         buttonText="Faça login com o Google"
@@ -34,7 +35,7 @@ export const LoginWithSocialMidia = () => {
                         buttonText="Logout"
                         onLogoutSuccess={logout}
                     />
-                </>
+                </> */}
             </div>
         </>
     )
