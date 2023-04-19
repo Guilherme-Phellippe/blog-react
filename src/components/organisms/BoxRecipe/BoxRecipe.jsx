efa5593adba4f32657e4ad45f8ee5035700d7021
guilher-phellippe <guilhermephellippe@outlook.com>
2023-04-19 15:53:33 -0300

    Resize image

--- src/components/organisms/BoxRecipe/BoxRecipe.jsx
+++ src/components/organisms/BoxRecipe/BoxRecipe.jsx
@@ -1,4 +1,3 @@
-// import { useEffect } from "react"
 import { Link } from 'react-router-dom';
 import { Img } from '../../atoms/Img';
 import { Info3MostViewedRecipes } from '../../atoms/Info3MostViewedRecipes';
@@ -12,8 +11,9 @@ export const BoxRecipe = ({ recipe, area, index }) => {
         <Link className={`rounded-lg relative cursor-pointer flex flex-col justify-center items-center overflow-hidden ${area} group`} to={`/recipe/${id}`}>
             <Img
                 className="absolute top-0 left-0 w-full h-full object-cover opacity-50 z-0 transition-opacity duration-300 ease-linear group-hover:opacity-80"
-                src={images_recipe[0].medium}
-                alt={`imagem de ${name_recipe}`} />
+                src={images_recipe[0].small}
+                alt={`imagem de ${name_recipe}`}
+            />
             <Info3MostViewedRecipes recipe={recipe} index={index} />
             <HoverInfo3MostViewedRecipes recipe={recipe} />
         </Link>
