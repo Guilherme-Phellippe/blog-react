d91581dd1c8f587aea2f74292e88c6971442a34b
guilher-phellippe <guilhermephellippe@outlook.com>
2023-04-19 15:58:43 -0300

    Resize image

--- src/components/organisms/BoxRecipe/BoxRecipe.jsx
+++ src/components/organisms/BoxRecipe/BoxRecipe.jsx
@@ -5,13 +5,17 @@ import { HoverInfo3MostViewedRecipes } from '../../molecules/HoverInfo3MostViewe
 
 export const BoxRecipe = ({ recipe, area, index }) => {
     const { images_recipe, name_recipe, id } = recipe;
-    // const image = index === 0 ? images_recipe[0].medium : images_recipe[0].small
+
+    const image = () =>{
+        if(window.innerWidth <= 700) return images_recipe[0].small
+        else return index === 0 ? images_recipe[0].medium : images_recipe[0].small
+    }
 
     return (
         <Link className={`rounded-lg relative cursor-pointer flex flex-col justify-center items-center overflow-hidden ${area} group`} to={`/recipe/${id}`}>
             <Img
                 className="absolute top-0 left-0 w-full h-full object-cover opacity-50 z-0 transition-opacity duration-300 ease-linear group-hover:opacity-80"
-                src={images_recipe[0].small}
+                src={image()}
                 alt={`imagem de ${name_recipe}`}
             />
             <Info3MostViewedRecipes recipe={recipe} index={index} />
