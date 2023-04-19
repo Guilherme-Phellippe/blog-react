3b1ca1a2e92bbbd240362db932a125b8f814d742
guilher-phellippe <guilhermephellippe@outlook.com>
2023-04-19 15:47:20 -0300

    fix a missing aria label in link

--- src/components/atoms/HeaderSocialMidia/index.jsx
+++ src/components/atoms/HeaderSocialMidia/index.jsx
@@ -11,16 +11,28 @@ export const SocialMidia = ({ hidden, isMobile }) => {
             >
                 <FaTiktok className={`my-2 ${isMobile ? 'text-s3 mx-4' : 'text-s1_7'} cursor-pointer rounded-full transition-all bg-black text-white`} />
             </a>
-            <a href="https://www.facebook.com/profile.php?id=100089888256334" target="_blank" rel="noreferrer"
-                aria-label="Esse link levará você ao Facebook">
+            <a
+                href="https://www.facebook.com/profile.php?id=100089888256334"
+                target="_blank"
+                rel="noreferrer"
+                aria-label="Esse link levará você ao Facebook"
+            >
                 <FaFacebook className={`my-2 ${isMobile ? 'text-s3 mx-4' : 'text-s1_7'} cursor-pointer rounded-full transition-all text-[#3b5998] bg-white`} />
             </a>
-            <a href="https://instagram.com/tem_saborofc/" target="_blank" rel="noreferrer"
-                aria-label="Esse link levará você ao Instagram">
+            <a
+                href="https://instagram.com/tem_saborofc/"
+                target="_blank"
+                rel="noreferrer"
+                aria-label="Esse link levará você ao Instagram"
+            >
                 <FaInstagram className={`my-2 ${isMobile ? 'text-s3 mx-4' : 'text-s1_7'} cursor-pointer rounded-full transition-all text-white bg-gradient-to-r from-yellow-500 via-red-500 to-pink-500`} />
             </a>
-            <a href="https://www.youtube.com/@temsaboroficial" target="_blank" rel="noreferrer"
-                aria-label="Esse link levará você ao Youtube">
+            <a
+                href="https://www.youtube.com/@temsaboroficial"
+                target="_blank"
+                rel="noreferrer"
+                aria-label="Esse link levará você ao Youtube"
+            >
                 <FaYoutube className={`my-2 ${isMobile ? 'text-s3 mx-4' : 'text-s1_7'} cursor-pointer rounded-full transition-all text-red-500 bg-white`} />
             </a>
         </div>
--- src/components/organisms/BoxRecipe/BoxRecipe.jsx
+++ src/components/organisms/BoxRecipe/BoxRecipe.jsx
@@ -1,3 +1,4 @@
+import { useEffect } from "react"
 import { Link } from 'react-router-dom';
 import { Img } from '../../atoms/Img';
 import { Info3MostViewedRecipes } from '../../atoms/Info3MostViewedRecipes';
@@ -5,14 +6,27 @@ import { HoverInfo3MostViewedRecipes } from '../../molecules/HoverInfo3MostViewe
 
 export const BoxRecipe = ({ recipe, area, index }) => {
     const { images_recipe, name_recipe, id } = recipe;
+    const image = index === 0 ? images_recipe[0].medium : images_recipe[0].small
+
+
+    useEffect(() => {
+        var link = document.createElement('link');
+        link.rel = "preload"
+        link.as = "image"
+        link.dataset.id = "preload"
+        link.href = image;
+        document.head.appendChild(link);
+    }, [image]);
+
+
 
     return (
         <Link className={`rounded-lg relative cursor-pointer flex flex-col justify-center items-center overflow-hidden ${area} group`} to={`/recipe/${id}`}>
             <Img
                 className="absolute top-0 left-0 w-full h-full object-cover opacity-50 z-0 transition-opacity duration-300 ease-linear group-hover:opacity-80"
-                src={index === 0 ? images_recipe[0].medium : images_recipe[0].small}
+                src={image}
                 alt={`imagem de ${name_recipe}`} />
-            <Info3MostViewedRecipes recipe={recipe} index={index}/>
+            <Info3MostViewedRecipes recipe={recipe} index={index} />
             <HoverInfo3MostViewedRecipes recipe={recipe} />
         </Link>
     )
