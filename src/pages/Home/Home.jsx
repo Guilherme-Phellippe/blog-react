d212d0c4ea6e4f6412a99f8f36f0287d602320bb
guilher-phellippe <guilhermephellippe@outlook.com>
2023-05-14 14:19:24 -0300

    add gdpr and ccpa

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -16,17 +16,18 @@ export default function Home() {
         </>
       }, [])}
       <CookieConsent
+        buttonWrapperClasses="w-full md:w-auto flex justify-center items-center mb-8 md:mb-0"
         location="bottom"
         buttonText="Aceito"
-        buttonClasses="text-s1_2"
+        buttonClasses="text-s1_3 font-bold w-1/2 md:w-auto md:px-8"
         cookieName="cookie-tem-sabor"
         expires={500}
       >
-        <p className="text-s1_7 text-center leading-10 text-white py-8">
-          A tem sabor está em conformidade com a GDPR e a CCPA e usa cookies para melhorar a sua experiência dentro do blog. 
+        <p className="text-s1_7 text-center leading-10 text-white pt-4">
+          A tem sabor está em conformidade com a GDPR e a CCPA e usa cookies para melhorar a sua experiência dentro do blog.
           Ao continuar navegando, você concorda com o uso de cookies, GDPR, CCPA,
           <a className="text-yellow-500 hover:text-yellow-200 cursor-pointer " href="/policy"> Politicas de privacidade</a>,
-          <a className="text-yellow-500 hover:text-yellow-200 cursor-pointer "href="/terms"> Termos de uso.</a>
+          <a className="text-yellow-500 hover:text-yellow-200 cursor-pointer " href="/terms"> Termos de uso.</a>
         </p>
       </CookieConsent>
 
