aaba9598f7650585004209e5b11b60bdb60d8c4e
guilher-phellippe <guilhermephellippe@outlook.com>
2023-05-18 19:41:29 -0300

    add new loading

--- src/components/atoms/Loading/Loading.jsx
+++ src/components/atoms/Loading/Loading.jsx
@@ -1,9 +1,9 @@
 export const Loading = () => {
     return (
-        <div className={`flex gap-1 justify-center absolute top-0 left-0 right-0 bottom-0 bg-[#fff5]`}>
-            <div className="w-5 h-5 max-w-[40px] max-h-[40px] rounded-full border-[2px] bg-color_orange animate-loading-1"></div>
-            <div className="w-5 h-5 max-w-[40px] max-h-[40px] rounded-full border-[2px] bg-color_orange animate-loading-2"></div>
-            <div className="w-5 h-5 max-w-[40px] max-h-[40px] rounded-full border-[2px] bg-color_orange animate-loading-3"></div>
+        <div className={`flex gap-1 items-center justify-center absolute top-0 left-0 right-0 bottom-0 bg-[#fff5]`}>
+            <div className="w-3 h-3 max-w-[15px] max-h-[15px] rounded-full bg-color_orange animate-loading-1"></div>
+            <div className="w-3 h-3 max-w-[15px] max-h-[15px] rounded-full bg-color_orange animate-loading-2"></div>
+            <div className="w-3 h-3 max-w-[15px] max-h-[15px] rounded-full bg-color_orange animate-loading-3"></div>
         </div>
     )
 }
