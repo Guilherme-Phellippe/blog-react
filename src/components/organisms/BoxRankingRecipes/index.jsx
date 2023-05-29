f6283193f2e32a2fae3632c1bc25ec8b06e99b4e
guilher-phellippe <guilhermephellippe@outlook.com>
2023-05-29 16:26:27 -0300

    add in article

--- src/components/organisms/BoxRankingRecipes/index.jsx
+++ src/components/organisms/BoxRankingRecipes/index.jsx
@@ -14,30 +14,29 @@ export const BoxRankingRecipes = ({ title, ranking }) => {
             {
                 !!filteredRanking.length && filteredRanking.map((recipe, index) => {
                     if (index < limitRecipes && index < 15) return (
-                        <>
-                            <ListRecipes
-                                key={recipe.id}
-                                recipe={recipe}
-                                classContainer="w-full h-[8rem] flex cursor-pointer border-b-[1px] border-x-[1px] border-solid hover:border-x-color_red p-2 hover:bg-background"
-                                classInfoContent="w-2/3 flex flex-col justify-center"
-                                classImgContent="w-1/3 h-full p-2"
-                            />
-                            <div className="w-full h-screen">
-                                <ins class="adsbygoogle"
-                                    style={{ display: "block" }}
-                                    data-ad-client="ca-pub-4781060024956035"
-                                    data-ad-slot="6974841302"
-                                    data-matched-content-ui-type="image_sidebyside"
-                                    data-matched-content-rows-num="4"
-                                    data-matched-content-columns-num="1"
-                                    data-ad-format="autorelaxed"
-                                ></ins>
-                            </div >
-                        </>
+                        <ListRecipes
+                            key={recipe.id}
+                            recipe={recipe}
+                            classContainer="w-full h-[8rem] flex cursor-pointer border-b-[1px] border-x-[1px] border-solid hover:border-x-color_red p-2 hover:bg-background"
+                            classInfoContent="w-2/3 flex flex-col justify-center"
+                            classImgContent="w-1/3 h-full p-2"
+                        />
                     )
                     return [];
                 })
+
             }
+            <div className="w-full h-screen">
+                <ins class="adsbygoogle"
+                    style={{ display: "block" }}
+                    data-ad-client="ca-pub-4781060024956035"
+                    data-ad-slot="6974841302"
+                    data-matched-content-ui-type="image_sidebyside"
+                    data-matched-content-rows-num="4"
+                    data-matched-content-columns-num="1"
+                    data-ad-format="autorelaxed"
+                ></ins>
+            </div >
             {ranking.length > limitRecipes ? <p data-id="see-more" onClick={() => setLimitRecipes(ranking.length)}>Veja mais</p> : ''}
 
 
