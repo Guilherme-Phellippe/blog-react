d8a0c9a3d6b7c127bc027b99c553a0808076295a
guilher-phellippe <guilhermephellippe@outlook.com>
2023-05-29 10:48:00 -0300

    add ads in header

--- src/components/templates/Header/Header.jsx
+++ src/components/templates/Header/Header.jsx
@@ -3,9 +3,18 @@ import { Menu } from '../../organisms/HeaderMenu/Menu'
 
 export const Header = () => {
     return (
-        <header className='w-full bg-color_orange flex flex-col items-center z-[999] mb-8 md:mb-16'>
-            <Bartop />
-            <Menu />
-        </header>
+        <>
+            <header className='w-full bg-color_orange flex flex-col items-center z-[999] mb-8 md:mb-16'>
+                <Bartop />
+                <Menu />
+            </header>
+            <ins class="adsbygoogle"
+                style={{ display: 'block' }}
+                data-ad-format="fluid"
+                data-ad-layout-key="-6o+ed+2i-1n-4w"
+                data-ad-client="ca-pub-4781060024956035"
+                data-ad-slot="1096599178"
+            ></ins>
+        </>
     )
 }
