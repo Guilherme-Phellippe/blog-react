effd51cd142b31cc21d2aabd1190ea16c67282a4
guilher-phellippe <guilhermephellippe@outlook.com>
2023-05-29 14:47:28 -0300

    add in article

--- src/components/organisms/BoxRankingRecipes/index.jsx
+++ src/components/organisms/BoxRankingRecipes/index.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import { ListRecipes } from '../../molecules/ListRecipes';
 import './styles.css'
+import { Adsense } from '../../molecules/Adsense';
 
 export const BoxRankingRecipes = ({ title, ranking }) => {
     const [limitRecipes, setLimitRecipes] = useState(8)
@@ -11,7 +12,7 @@ export const BoxRankingRecipes = ({ title, ranking }) => {
         <div className={`container-ranking pb-12`}>
             <h2>{title}</h2>
             {
-                filteredRanking.length && filteredRanking.map((recipe, index) => {
+                !!filteredRanking.length && filteredRanking.map((recipe, index) => {
                     if (index < limitRecipes && index < 15) return (
                         <ListRecipes
                             key={recipe.id}
@@ -26,13 +27,7 @@ export const BoxRankingRecipes = ({ title, ranking }) => {
             }
             {ranking.length > limitRecipes ? <p data-id="see-more" onClick={() => setLimitRecipes(ranking.length)}>Veja mais</p> : ''}
 
-            <ins class="adsbygoogle"
-                style={{ display: "block" }}
-                data-ad-format="autorelaxed"
-                data-ad-client="ca-pub-4781060024956035"
-                data-ad-slot="6974841302"
-            ></ins>
-
+            <Adsense slot="6974841302" format="autorelaxed" />
 
         </div>
     )
