fe13bafc15242ab696f4bc3fb743b7236966ff6d
guilher-phellippe <guilhermephellippe@outlook.com>
2023-05-30 15:14:51 -0300

    adsense recipe page

--- src/components/organisms/InfoRecipeContent/index.jsx
+++ src/components/organisms/InfoRecipeContent/index.jsx
@@ -10,10 +10,10 @@ import { LikeComentsSaveButtons } from '../../molecules/LikeComentSaveButtons'
 export const InfoRecipeContent = ({ recipe }) => {
     const { name_recipe, nmr_hearts, images_recipe, ing, stuffing_ing, prepareMode, nmr_saved } = recipe
 
-    // useEffect(() => {
-    //     // GOOGLE ADSENSE 
-    //     window.location.hostname !== 'localhost' && (window.adsbygoogle = window.adsbygoogle || []).push({});
-    // }, [])
+    useEffect(() => {
+        // GOOGLE ADSENSE 
+        window.location.hostname !== 'localhost' && (window.adsbygoogle = window.adsbygoogle || []).push({});
+    }, [])
 
     return (
         <div className="w-full flex">
@@ -28,7 +28,7 @@ export const InfoRecipeContent = ({ recipe }) => {
                     <LikeComentsSaveButtons nmr_hearts={nmr_hearts} nmr_saved={nmr_saved} />
                 </div>
             </div>
-            {/* <div className="w-1/3">
+            <div className="w-1/3">
                 <ins class="adsbygoogle"
                     style={{ display: 'block', textAlign: 'center', width: '100%' }}
                     data-ad-layout="in-article"
@@ -36,7 +36,7 @@ export const InfoRecipeContent = ({ recipe }) => {
                     data-ad-client="ca-pub-4781060024956035"
                     data-ad-slot="1064809587"
                 ></ins>
-            </div> */}
+            </div>
         </div>
     )
 }
