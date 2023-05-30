511d4b97423d66e01f154a8b29f17246877d610f
guilher-phellippe <guilhermephellippe@outlook.com>
2023-05-30 15:39:03 -0300

    adsense recipe page

--- src/components/organisms/InfoRecipeContent/index.jsx
+++ src/components/organisms/InfoRecipeContent/index.jsx
@@ -21,13 +21,15 @@ export const InfoRecipeContent = ({ recipe }) => {
             <div className="w-full md:w-2/3 p-4">
                 <InfoRecipeHeader nmr_hearts={nmr_hearts} name_recipe={name_recipe} />
                 <CarouselMidiasContent name_recipe={name_recipe} img={images_recipe} />
-                <ins className="adsbygoogle"
-                    style={{ display: 'block' }}
-                    data-ad-client="ca-pub-4781060024956035"
-                    data-ad-slot="9346456414"
-                    data-ad-format="auto"
-                    data-full-width-responsive="true"
-                ></ins>
+                <div className="px-4">
+                    <ins className="adsbygoogle"
+                        style={{ display: 'block' }}
+                        data-ad-client="ca-pub-4781060024956035"
+                        data-ad-slot="9346456414"
+                        data-ad-format="auto"
+                        data-full-width-responsive="true"
+                    ></ins>
+                </div>
                 <PreparationInformation recipe={recipe} />
                 <RecipeChefInfo recipe={recipe} />
                 <IngredientsList ing={ing} stuffing_ing={stuffing_ing} />
