4ff8bcb1645879dca0d59eb3c1862207fbc7ae81
guilher-phellippe <guilhermephellippe@outlook.com>
2023-05-30 15:20:36 -0300

    adsense recipe page

--- src/components/organisms/InfoRecipeContent/index.jsx
+++ src/components/organisms/InfoRecipeContent/index.jsx
@@ -30,13 +30,13 @@ export const InfoRecipeContent = ({ recipe }) => {
                 </div>
             </div>
             <div className="w-1/3">
-                <ins class="adsbygoogle"
+                {/* <ins class="adsbygoogle"
                     style={{ display: 'block', textAlign: 'center', width: '100%' }}
                     data-ad-layout="in-article"
                     data-ad-format="fluid"
                     data-ad-client="ca-pub-4781060024956035"
                     data-ad-slot="1064809587"
-                ></ins>
+                ></ins> */}
                 <ins class="adsbygoogle"
                     style={{ display: 'block' }}
                     data-ad-client="ca-pub-1234567890123456"
