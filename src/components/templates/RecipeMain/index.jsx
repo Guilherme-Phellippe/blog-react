e0d7625f184e89bdc4de7c632ed81419826c4578
guilher-phellippe <guilhermephellippe@outlook.com>
2023-06-05 10:24:04 -0300

    add Helmet meta tags

--- src/components/templates/RecipeMain/index.jsx
+++ src/components/templates/RecipeMain/index.jsx
@@ -52,13 +52,17 @@ export default function RecipeMain() {
     return (
         <div className="w-full max-w-[1500px] mx-auto">
             <Helmet>
-                <meta property="og:type" content="website" />
-                <meta property="og:image" content={recipe ? recipe.images_recipe[0].small : ""} />
-                <meta property="og:image:width" content="1200" />
-                <meta property="og:image:height" content="650" />
-                <meta property="og:title" content={recipe && recipe.name_recipe} />
-                <meta property="og:description" content="Conheça essa deliciosa receita no nosso blog Tem sabor" />
-
+                {
+                    recipe &&
+                    <>
+                        <meta property="og:type" content="website" />
+                        <meta property="og:image" content={recipe.images_recipe[0].small} />
+                        <meta property="og:image:width" content="1200" />
+                        <meta property="og:image:height" content="650" />
+                        <meta property="og:title" content={recipe.name_recipe} />
+                        <meta property="og:description" content="Conheça essa deliciosa receita no nosso blog Tem sabor" />
+                    </>
+                }
             </Helmet>
 
             {
