014e7c256ad24e9d80d341df9580a7d341f77a72
guilher-phellippe <guilhermephellippe@outlook.com>
2023-06-29 20:22:28 -0300

    add Ads in Feed

--- src/components/organisms/Feed/index.jsx
+++ src/components/organisms/Feed/index.jsx
@@ -1,61 +1,67 @@
 import 'moment/locale/pt-br';
 
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 
 import { FeedRecipes } from '../../molecules/FeedRecipes';
 import { FeedTip } from '../../molecules/FeedTips';
 import { Loading } from '../../atoms/Loading/Loading';
 
-// function AdsInFeed({ index }) {
+function AdsInFeed({ index }) {
 
-//    const viewer = useCallback(() => {
-//         switch (index) {
-//             case 7 || 23 || 35:
-//                 console.log("index: " + index, 7, 23, 35)
-//                 return <ins
-//                     className="adsbygoogle"
-//                     style={{ display: "block" }}
-//                     data-ad-format="fluid"
-//                     data-ad-layout-key="+3v+pt+4r-a-41"
-//                     data-ad-client="ca-pub-4781060024956035"
-//                     data-ad-slot="1096599178"></ins>
+    useEffect(() => {
+        // GOOGLE ADSENSE 
+        window.location.hostname !== 'localhost' &&
+            (window.adsbygoogle = window.adsbygoogle || []).push({});
+    }, [])
 
-//             case 11 || 27 || 39:
-//                 console.log("index: " + index, 11, 27, 39)
-//                 return <ins
-//                     className="adsbygoogle"
-//                     style={{ display: "block" }}
-//                     data-ad-format="fluid"
-//                     data-ad-layout-key="-69+ed+2i-1n-4w"
-//                     data-ad-client="ca-pub-4781060024956035"
-//                     data-ad-slot="2675977957"></ins>
-//             case 15 || 31 || 43:
-//                 console.log("index: " + index, 15, 31, 43)
-//                 return <ins
-//                     className="adsbygoogle"
-//                     style={{ display: "block" }}
-//                     data-ad-format="fluid"
-//                     data-ad-layout-key="-5z+ed+2i-1n-4w"
-//                     data-ad-client="ca-pub-4781060024956035"
-//                     data-ad-slot="1390816442"></ins>
-//             default:
-//                 console.log("index: " + index, "default")
-//                 return <ins
-//                     className="adsbygoogle"
-//                     style={{ display: "block" }}
-//                     data-ad-format="fluid"
-//                     data-ad-layout-key="-5z+ed+2i-1n-4w"
-//                     data-ad-client="ca-pub-4781060024956035"
-//                     data-ad-slot="4072766205"></ins>
-//         }
-//     }, [index])
+    const viewer = useCallback(() => {
+        switch (index) {
+            case 7 || 23 || 35:
+                console.log("index: " + index, 7, 23, 35)
+                return <ins
+                    className="adsbygoogle"
+                    style={{ display: "block" }}
+                    data-ad-format="fluid"
+                    data-ad-layout-key="+3v+pt+4r-a-41"
+                    data-ad-client="ca-pub-4781060024956035"
+                    data-ad-slot="1096599178"></ins>
+
+            case 11 || 27 || 39:
+                console.log("index: " + index, 11, 27, 39)
+                return <ins
+                    className="adsbygoogle"
+                    style={{ display: "block" }}
+                    data-ad-format="fluid"
+                    data-ad-layout-key="-69+ed+2i-1n-4w"
+                    data-ad-client="ca-pub-4781060024956035"
+                    data-ad-slot="2675977957"></ins>
+            case 15 || 31 || 43:
+                console.log("index: " + index, 15, 31, 43)
+                return <ins
+                    className="adsbygoogle"
+                    style={{ display: "block" }}
+                    data-ad-format="fluid"
+                    data-ad-layout-key="-5z+ed+2i-1n-4w"
+                    data-ad-client="ca-pub-4781060024956035"
+                    data-ad-slot="1390816442"></ins>
+            default:
+                console.log("index: " + index, "default")
+                return <ins
+                    className="adsbygoogle"
+                    style={{ display: "block" }}
+                    data-ad-format="fluid"
+                    data-ad-layout-key="-5z+ed+2i-1n-4w"
+                    data-ad-client="ca-pub-4781060024956035"
+                    data-ad-slot="4072766205"></ins>
+        }
+    }, [index])
 
 
-//     return (
-//         viewer()
-//     )
+    return (
+        viewer()
+    )
 
-// }
+}
 
 
 export default function Feed({ contents, valueSearch }) {
@@ -77,11 +83,7 @@ export default function Feed({ contents, valueSearch }) {
     }, [contents, listRecipeForRemove]);
 
 
-    useEffect(() => {
-        // GOOGLE ADSENSE 
-        window.location.hostname !== 'localhost' &&
-            (window.adsbygoogle = window.adsbygoogle || []).push({});
-    }, [])
+
 
     const handleIdForAddListRemove = ({ currentTarget }) => {
         setListRecipeForRemove(list => [...list, currentTarget.id])
@@ -93,14 +95,7 @@ export default function Feed({ contents, valueSearch }) {
                 return (
                     ((index) % 2) === 0 ?
                         <div className="grid min-h-[20rem] bg-white my-4">
-                            {/* <AdsInFeed index={index} /> */}
-                            <ins
-                                className="adsbygoogle"
-                                style={{ display: "block" }}
-                                data-ad-format="fluid"
-                                data-ad-layout-key="-5z+ed+2i-1n-4w"
-                                data-ad-client="ca-pub-4781060024956035"
-                                data-ad-slot="4072766205"></ins>
+                            <AdsInFeed index={index} />
                         </div>
                         :
                         content.name_recipe ?
