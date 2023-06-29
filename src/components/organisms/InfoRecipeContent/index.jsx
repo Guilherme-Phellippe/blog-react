ee7cf586ae64af4c1f6258d161ef68454f815bae
guilher-phellippe <guilhermephellippe@outlook.com>
2023-06-29 16:07:13 -0300

    add new Ad 2.0

--- src/components/organisms/InfoRecipeContent/index.jsx
+++ src/components/organisms/InfoRecipeContent/index.jsx
@@ -1,4 +1,4 @@
-import { Suspense, lazy, useEffect } from 'react';
+import { Suspense, lazy } from 'react';
 import { Loading } from '../../atoms/Loading/Loading';
 import { ListRecipeComments } from '../../molecules/ListRecipeComments';
 
