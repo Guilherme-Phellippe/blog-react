99150e904c1d761459f5f6f00bd99dc61fd8f9a5
guilher-phellippe <guilhermephellippe@outlook.com>
2023-06-29 10:25:28 -0300

    show 'principal on prepare mode recipe'

--- src/components/atoms/PrepareMode/index.jsx
+++ src/components/atoms/PrepareMode/index.jsx
@@ -8,16 +8,18 @@ export default function PrepareMode({ prepareMode, type_prepare_mode }) {
             <h2 className='text-s2 text-color_orange text-center font-bold my-4 bg-color_orange/20 w-full p-4'>MODO DE PREPARO</h2>
             {
                 createListIngAdd(type_prepare_mode, newPrepareMode).map((item, index) =>
-                    <div key={item.name+item.count} className="w-full flex flex-col justify-center items-start">
+                    <div key={item.name + item.count} className="w-full flex flex-col justify-center items-start">
                         {
-                             index === 0 && item.name !== "Principal" &&
-                            <h3 className="text-color_orange bg-color_orange/20 p-2 rounded-xl text-s1_7 font-bold">{item.name}:</h3>
+                            item.name !== "Principal" && index !== 0 &&
+                            <h3 className="text-color_orange bg-color_orange/20 p-2 rounded-xl text-s1_7 font-bold">
+                                {item.name}:
+                            </h3>
                         }
                         <ul className='w-full p-0 md:p-4 flex flex-col'>
                             {
                                 item.ing.map((i, index) =>
                                     <li
-                                        key={i+index}
+                                        key={i + index}
                                         className="text-s1_7 list-none text-color_text_black tracking-wide text-center md:text-left flex items-start gap-4 my-4 leading-10"
                                     >
                                         <span className="text-s1_5 bg-color_orange text-white px-4 py-3 rounded-full">{index + 1}</span>
