d8e6e38f1e86901e158b51d64ef6b9e2950822af
guilher-phellippe <guilhermephellippe@outlook.com>
2023-06-29 20:25:22 -0300

    add Ads in Feed

--- src/components/organisms/Feed/index.jsx
+++ src/components/organisms/Feed/index.jsx
@@ -93,7 +93,7 @@ export default function Feed({ contents, valueSearch }) {
         <div className='min-h-screen relative'>
             {feed.length ? feed.map((content, index) => {
                 return (
-                    ((index) % 2) === 0 ?
+                    ((index+1) % 4) === 0 ?
                         <div className="grid min-h-[20rem] bg-white my-4">
                             <AdsInFeed index={index} />
                         </div>
