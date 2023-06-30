1cfc60c9a1e542a9c17d19abc1d7caf81006b5ba
guilher-phellippe <guilhermephellippe@outlook.com>
2023-06-30 09:49:05 -0300

    add Ads in Feed

--- src/components/organisms/RecipeSimilarContent/index.jsx
+++ src/components/organisms/RecipeSimilarContent/index.jsx
@@ -14,9 +14,11 @@ export default function RecipeSimilarContent({ name_search }) {
             setRecipes(data)
         }
         dataFetch(api)
+        console.log(window.adsbygoogle)
         // GOOGLE ADSENSE 
         window.location.hostname !== 'localhost' &&
             (window.adsbygoogle = window.adsbygoogle || []).push({});
+            
     }, []);
 
 
@@ -59,6 +61,7 @@ export default function RecipeSimilarContent({ name_search }) {
                     data-matched-content-columns-num={window.innerWidth < 700 ? "2" : "4"}
                     data-ad-format="autorelaxed"
                 ></ins>
+                {console.log("anuncio 4")}
             </div>
 
             <div className="w-full bg-white rounded-md">
