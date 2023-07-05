44336d51267c07bad65f495b3eb9819f56197682
guilher-phellippe <guilhermephellippe@outlook.com>
2023-07-05 16:52:40 -0300

    remove window.onsignal to configuration

--- src/libs/oneSignal.config.js
+++ src/libs/oneSignal.config.js
@@ -1,8 +1,6 @@
 import OneSignal from 'react-onesignal';
 
 export const initOneSignal = async () => {
-    window.OneSignal = window.OneSignal || [];
-
     await OneSignal.init(
         {
             appId: "1fc3feb0-617d-4599-b8d8-cd5c995aca0c",
