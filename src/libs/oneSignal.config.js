68028ceb04c4ec305caca32af845f3c14396f2c8
guilher-phellippe <guilhermephellippe@outlook.com>
2023-07-05 17:11:44 -0300

    get subscription

--- src/libs/oneSignal.config.js
+++ src/libs/oneSignal.config.js
@@ -2,8 +2,13 @@ import OneSignal from 'react-onesignal';
 
 export const initOneSignal = async () => {
     await OneSignal.init({ appId: "1fc3feb0-617d-4599-b8d8-cd5c995aca0c" });
+    const subscription = await OneSignal.getSubscription();
+    const externalUserId = await OneSignal.getExternalUserId();
+    const userId = await OneSignal.getUserId();
+    const tags = await OneSignal.getTags();
 
-    console.log(await OneSignal.getSubscription())
-
-    OneSignal.getNotificationPermission();
+    console.log(subscription)
+    console.log(externalUserId)
+    console.log(userId)
+    console.log(tags)
 }
