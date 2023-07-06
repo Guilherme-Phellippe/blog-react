4b4d76d7ac3ce1f2ab400ece2768888623d2d962
guilher-phellippe <guilhermephellippe@outlook.com>
2023-07-06 16:33:07 -0300

    analyse the result the init one signal 01

--- src/libs/oneSignal.config.js
+++ src/libs/oneSignal.config.js
@@ -1,5 +1,11 @@
 import OneSignal from 'react-onesignal';
 
 export const initOneSignal = async () => {
-  await OneSignal.init({ appId: "1fc3feb0-617d-4599-b8d8-cd5c995aca0c" })
+  await OneSignal.init({ appId: "1fc3feb0-617d-4599-b8d8-cd5c995aca0c" }).then(res =>{
+    console.log(res)
+  });
+
+  
+
+
 }
