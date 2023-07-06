1e7a2eb74ebcbb9e0532eb3938a7f54f46b2b7d0
guilher-phellippe <guilhermephellippe@outlook.com>
2023-07-06 16:45:27 -0300

    permission user notification push

--- src/libs/oneSignal.config.js
+++ src/libs/oneSignal.config.js
@@ -3,9 +3,11 @@ import OneSignal from 'react-onesignal';
 export const initOneSignal = async () => {
   await OneSignal.init({ appId: "1fc3feb0-617d-4599-b8d8-cd5c995aca0c" })
   
-  const users = await OneSignal.getUserId();
+  const userDevice = await OneSignal.getUserId();
+  if(userDevice){
+    console.log(Notification.permission)
+  }
 
-  console.log(users)
 
 
 }
