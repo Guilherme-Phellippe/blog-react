0b9db410377fd0e1f15a71949ccabb21c45a02ab
guilher-phellippe <guilhermephellippe@outlook.com>
2023-07-07 17:49:34 -0300

    testing google login

--- src/modals/LoginWithGoogleModal/index.js
+++ src/modals/LoginWithGoogleModal/index.js
@@ -5,16 +5,15 @@
  * @param {string} buttonText 
  * @returns 
  */
-export const loginWithGoogleModal = (text , type, buttonText) => {
+export const loginWithGoogleModal = () => {
     const root = document.querySelector("#root")
-    const color = type === 0 ? "red-500" : type === 1 ? "color_orange" : "green-700"
 
     return new Promise((resolve, reject) => {
         const container = document.createElement('div');
-        container.setAttribute("class", "fixed top-0 left-0 z-[999] w-screen h-screen bg-black/30 grid place-items-center");
+        container.setAttribute("class", "fixed bottom-0 left-0 z-[999] w-screen h-screen");
 
         const box = document.createElement('div');
-        box.setAttribute('class', `min-w-[20rem] max-w-[80%] md:max-w-2/3 min-h-[12rem] bg-white p-2 rounded-xl flex flex-col justify-between border-[1px] border-${color}`)
+        box.setAttribute('class', `min-w-[20rem] max-w-[80%] md:max-w-2/3 min-h-[12rem] bg-white p-2 rounded-xl flex flex-col justify-between border-[1px]`)
 
         const contentTop = document.createElement('div');
         contentTop.setAttribute('class', "w-full flex-col");
@@ -23,37 +22,8 @@ export const loginWithGoogleModal = (text , type, buttonText) => {
         contentBottom.setAttribute('class', "w-full flex justify-center gap-8 border-t p-4")
 
         const title = document.createElement("h2");
-        title.setAttribute("class", `text-${color} bg-${color}/20 font-bold text-center text-s1_5 py-3 px-8 rounded-t-xl`)
-        title.innerText = "BLOG TEM SABOR"
-
-        const message = document.createElement("p");
-        message.setAttribute("class", `text-color_text_black text-center text-s1_5 p-8 rounded-t-xl p-4`)
-        message.innerText = text
-
-        const buttonClose = document.createElement('button')
-        buttonClose.setAttribute("class", `text-s1_3 p-2 px-4 text-color_text_black border rounded-xl`)
-        buttonClose.innerHTML = buttonText ? "Fechar" : "OK"
-        buttonClose.addEventListener('click', () => {
-            root.removeChild(container)
-            resolve(false)
-        })
-
-        const buttonConfirm = document.createElement('button')
-        buttonConfirm.setAttribute("class", `bg-${color} text-s1_3 p-2 px-6 text-white border rounded-xl`)
-        buttonConfirm.innerHTML = buttonText
-        buttonConfirm.addEventListener('click', () => {
-            root.removeChild(container)
-            resolve(true)
-        })
-
-        contentTop.appendChild(title)
-        contentTop.appendChild(message)
-        contentBottom.appendChild(buttonClose)
-        buttonText && contentBottom.appendChild(buttonConfirm)
-        box.appendChild(contentTop)
-        box.appendChild(contentBottom)
-        container.appendChild(box)
-
+        title.setAttribute("class", `text-color_text_black font-bold text-center text-s1_5 py-3 px-8 rounded-t-xl`)
+        title.innerText = "Faça login com o google"
 
         root.appendChild(container)
     })
