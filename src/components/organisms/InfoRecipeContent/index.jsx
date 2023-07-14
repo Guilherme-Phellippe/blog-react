286d76aa5584c6b85af700a321469cf7028a9efe
guilher-phellippe <guilhermephellippe@outlook.com>
2023-07-14 15:47:54 -0300

    add banner ads in side carroucel

--- src/components/organisms/InfoRecipeContent/index.jsx
+++ src/components/organisms/InfoRecipeContent/index.jsx
@@ -88,14 +88,15 @@ export default function InfoRecipeContent({ recipe, showContentAfterScroll }) {
                 }
             </div>
 
-            <div className="hidden md:block md:w-1/3 border border-red-500">
+            <div className="hidden md:block md:w-1/3 p-4">
                 <LineAds>
                     <ins className="adsbygoogle"
-                        style={{display:"block"}}
+                        style={{ display: "block" }}
                         data-ad-client="ca-pub-4781060024956035"
                         data-ad-slot="5163292498"
                         data-ad-format="auto"
-                        data-full-width-responsive="true"></ins>
+                        data-full-width-responsive="true"
+                    ></ins>
                 </LineAds>
             </div>
         </div>
