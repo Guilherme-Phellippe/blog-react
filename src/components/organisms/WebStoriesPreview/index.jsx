a189272cc92170e83b6b2402df417608fd444e4a
guilher-phellippe <guilhermephellippe@outlook.com>
2023-07-20 15:24:02 -0300

    add static html

--- src/components/organisms/WebStoriesPreview/index.jsx
+++ src/components/organisms/WebStoriesPreview/index.jsx
@@ -18,7 +18,7 @@ export default function WebStoriesPreview() {
         <div className="flex gap-6 p-8 w-full bg-white overflow-x-auto ">
             {
                 stories.map(story =>
-                    <a key={story.id} href={`https://stories.temsabor.blog/story/${story.slug}`}>
+                    <a key={story.id} href={`https://temsabor.blog/stories/${story.slug}`}>
                         <div className="w-[12rem] flex flex-col items-center gap-4">
                             <div className="w-[8rem] h-[8rem] rounded-full overflow-hidden cursor-pointer opacity-80 hover:opacity-100 border-[3px] border-blue-600">
                                 <Img imgs={story.story_poster_portrait_src} alt={`Essa é imagem representa: ${story.story_title}`} />
