45bf561d63cbb6124088566b9929af0ff7d0fdf9
guilher-phellippe <guilhermephellippe@outlook.com>
2023-07-20 15:17:08 -0300

    add static html

---
+++ public/Stories/index.js
@@ -0,0 +1,21 @@
+// JS
+let player;
+
+function initializePlayer() {
+    player = window.document.querySelector("amp-story-player");
+    if (player.isReady) {
+        player.addEventListener("amp-story-player-close", () => {
+            window.location.href = "https://temsabor.blog/"
+        });
+        return;
+    }
+
+    player.addEventListener("ready", () => {
+        player.addEventListener("amp-story-player-close", () => {
+            window.location.href = "https://temsabor.blog/"
+        });
+    });
+}
+
+
+initializePlayer();
