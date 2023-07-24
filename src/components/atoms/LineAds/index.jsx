880b22e7990d14fd9365a7b35070099475c3b762
guilher-phellippe <guilhermephellippe@outlook.com>
2023-07-24 17:01:22 -0300

    remove window.innerWidth > 764 ? none:block in block ad

--- src/components/atoms/LineAds/index.jsx
+++ src/components/atoms/LineAds/index.jsx
@@ -3,7 +3,7 @@ export default function LineAds({ children }) {
         <div className="my-8">
             <div className="flex justify-center opacity-25">
                 <span className="w-full bg-zinc-900/50 h-[1px]"></span>
-                <span className="mx-2 -translate-y-1/2">Anúncio</span>
+                <span className="mx-2 -translate-y-1/2">Publicidade</span>
                 <span className="w-full bg-zinc-900/50 h-[1px]"></span>
             </div>
             {children}
