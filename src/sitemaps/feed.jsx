d4efc1065e47db797e7d3e3ca11420b8c3bfd5bd
guilher-phellippe <guilhermephellippe@outlook.com>
2023-07-24 10:50:18 -0300

    try add rss in react

--- src/sitemaps/feed.jsx
+++ src/sitemaps/feed.jsx
@@ -1,5 +1,6 @@
 export default function SiteMapFeeds() {
     return (
+        `
         <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns: xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns: news="http://www.google.com/schemas/sitemap-news/0.9" xsi: schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd">
 
             <url>
@@ -11,10 +12,10 @@ export default function SiteMapFeeds() {
                     </news:publication>
                     <news:publication_date>2023-07-24T09:38:39+00:00</news:publication_date>
                     <news:title>
-                        {`<![CDATA[ RECEITA DE QUEIJADINHA ]]>`}
+                        < ![CDATA[RECEITA DE QUEIJADINHA]]>
                     </news:title>
                     <news:keywords>
-                        {`<![CDATA[ Queijadinha, fácil, rápida ]]>`}
+                        < ![CDATA[Queijadinha, fácil, rápida]] >
                     </news:keywords>
                 </news:news>
                 <priority>1.0</priority>
@@ -22,5 +23,6 @@ export default function SiteMapFeeds() {
             </url>
 
         </urlset>
+        `
     )
 }
