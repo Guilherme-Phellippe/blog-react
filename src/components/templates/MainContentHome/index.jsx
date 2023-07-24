ba5d866bc2ee20e71f565ce4378b43171ecdad49
guilher-phellippe <guilhermephellippe@outlook.com>
2023-07-24 09:35:07 -0300

    remove ad home

--- src/components/templates/MainContentHome/index.jsx
+++ src/components/templates/MainContentHome/index.jsx
@@ -89,7 +89,7 @@ export default function MainContentHome() {
                 !valueSearch &&
                     <ins
                         className="adsbygoogle"
-                        style={{ display: "block", margin:"1rem auto" }}
+                        style={{ display: "flex", justifyContent: "center" }}
                         data-ad-client="ca-pub-4781060024956035"
                         data-ad-slot="2090078650"
                         data-ad-format="auto"
