c8d17100caae73c35d994c888d1c67c30787f9d6
guilher-phellippe <guilhermephellippe@outlook.com>
2023-07-24 23:04:17 -0300

    testing block Ad

--- src/components/organisms/InfoRecipeContent/index.jsx
+++ src/components/organisms/InfoRecipeContent/index.jsx
@@ -80,21 +80,22 @@ export default function InfoRecipeContent({ recipe, showContentAfterScroll }) {
                                     </LineAds>
                                 </Suspense>
                             </div>
+                            {/* <div className="hidden md:block md:w-1/3 p-4">
+                                <LineAds isLastComponentRendering={true}>
+                                    <ins className="adsbygoogle"
+                                        style={{ display: "block" }}
+                                        data-ad-client="ca-pub-4781060024956035"
+                                        data-ad-slot="5163292498"
+                                        data-ad-format="auto"
+                                        data-full-width-responsive="true"
+                                    ></ins>
+                                </LineAds>
+                            </div> */}
                         </>
                     }
                 </div>
 
-                <div className="hidden md:block md:w-1/3 p-4">
-                    <LineAds isLastComponentRendering={true}>
-                        <ins className="adsbygoogle"
-                            style={{ display: "block" }}
-                            data-ad-client="ca-pub-4781060024956035"
-                            data-ad-slot="5163292498"
-                            data-ad-format="auto"
-                            data-full-width-responsive="true"
-                        ></ins>
-                    </LineAds>
-                </div>
+
 
             </div>
 
