06b2cef7be6d531032d05980789ec1d57c219649
guilher-phellippe <guilhermephellippe@outlook.com>
2023-07-25 11:53:03 -0300

    testing block Ad

--- src/pages/Recipe/index.jsx
+++ src/pages/Recipe/index.jsx
@@ -1,4 +1,4 @@
-import { lazy, useEffect, useState } from 'react';
+import { lazy, useEffect } from 'react';
 import { HomeProvider } from '../../contexts/Home/HomeProvider'
 import { initOneSignal } from '../../libs/oneSignal.config';
 import LoginWithSocialMidiaModal from '../../modals/LoginWithSocialMidiaModal';
@@ -8,32 +8,16 @@ const Footer = lazy(() => import("../../components/templates/Footer/Footer"))
 const Main = lazy(() => import('../../components/templates/RecipeMain'))
 
 export default function Recipe() {
-    const [showContentAfterScroll, setShowContentAfterScroll] = useState(false);
-
     useEffect(() => {
         initOneSignal();
     }, []);
 
 
-    useEffect(() => {
-        //THIS CODE IS SHOW SECTION IF USER SCROLLING PAGE
-        const handleScroll = () => window.scrollY > 20 && setShowContentAfterScroll(true)
-
-        window.addEventListener('scroll', handleScroll)
-
-        return () => {
-            window.removeEventListener('scroll', handleScroll)
-        }
-    }, [])
-
     return (
         <HomeProvider>
             <Header />
-            <Main showContentAfterScroll={showContentAfterScroll} />
-            {
-                showContentAfterScroll && <Footer />
-
-            }
+            <Main />
+            <Footer />
             <LoginWithSocialMidiaModal />
         </HomeProvider>
     )
