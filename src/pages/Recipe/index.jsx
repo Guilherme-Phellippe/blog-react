7395b8f8a4273db0689df2d111cc6738fc3a5ef2
guilher-phellippe <guilhermephellippe@outlook.com>
2023-07-27 16:47:10 -0300

    change the way load moment

--- src/pages/Recipe/index.jsx
+++ src/pages/Recipe/index.jsx
@@ -1,6 +1,5 @@
 import { Suspense, lazy, useContext, useEffect } from 'react';
 import { UserContext } from '../../contexts/userProvider';
-import { initOneSignal } from '../../libs/oneSignal.config';
 import { HomeProvider } from '../../contexts/Home/HomeProvider';
 import { Loading } from '../../components/atoms/Loading/Loading';
 
@@ -8,12 +7,13 @@ const Header = lazy(() => import("../../components/templates/Header/Header"))
 const Footer = lazy(() => import("../../components/templates/Footer/Footer"))
 const Main = lazy(() => import('../../components/templates/RecipeMain'))
 const LoginWithSocialMidiaModal = lazy(() => import('../../modals/LoginWithSocialMidiaModal'))
+const initOneSignal = lazy(()=> import('../../libs/oneSignal.config'));
 
 export default function Recipe() {
     const { user } = useContext(UserContext)
 
     useEffect(() => {
-        window.location.hostname !== 'localhost' && window.addEventListener("load", () => initOneSignal())
+        window.location.hostname !== 'localhost' && window.addEventListener("load", () => initOneSignal());
     }, []);
 
     return (
