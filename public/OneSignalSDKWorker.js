c7f7b76b83a606e3201596b4e4241d5c1e0163a3
guilher-phellippe <guilhermephellippe@outlook.com>
2023-07-27 13:10:15 -0300

    Testing speed recipe page

--- public/OneSignalSDKWorker.js
+++ public/OneSignalSDKWorker.js
@@ -1,5 +1,5 @@
+console.log("Eu estou sendo chamado?")
 window.addEventListener("load", ()=>{
     // eslint-disable-next-line no-undef
     importScripts('https://cdn.onesignal.com/sdks/OneSignalSDKWorker.js');
-    console.log("Eu estou sendo chamado?")
 })
