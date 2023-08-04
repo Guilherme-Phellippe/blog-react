18614708e6006ed68a7485d6629fd190b2d4d744
Guilherme Phellippe <guilhermephellippe@outlook.com>
2023-08-04 17:05:18 -0300

    add new space to ad from alk

--- src/components/organisms/InfoRecipeContent/index.jsx
+++ src/components/organisms/InfoRecipeContent/index.jsx
@@ -1,4 +1,4 @@
-import { Suspense, lazy } from 'react';
+import { Suspense, lazy, useEffect, useRef } from 'react';
 import { Loading } from '../../atoms/Loading/Loading';
 import { ListRecipeComments } from '../../molecules/ListRecipeComments';
 import RecipeSimilarContent from '../RecipeSimilarContent';
@@ -14,6 +14,46 @@ const LikeComentsSaveButtons = lazy(() => import("../../molecules/LikeComentSave
 
 
 export default function InfoRecipeContent({ recipe, user }) {
+    const refContainerAd = useRef();
+
+    const handleContainerAdOnViewport = ()=>{
+        function isDivIsViewport(){
+            const heightAd = refContainerAd.current.querySelector("div#container-ads").offsetHeight;
+            const rect = refContainerAd?.current.getBoundingClientRect();
+            return [rect.top <= 0,  rect.bottom <= heightAd ]
+        }
+
+
+        if(refContainerAd.current){
+            const divAd = refContainerAd.current.querySelector("div#container-ads");
+            if(isDivIsViewport()[0] && !isDivIsViewport()[1]) {
+                divAd.classList.add(`w-[${refContainerAd.current.offsetWidth}px]`)
+                divAd.classList.add("fixed");
+                divAd.classList.add("top-0");
+                divAd.classList.remove("sticky");
+                divAd.classList.remove("bottom-0");
+                divAd.classList.remove("absolute");
+            }else if(isDivIsViewport()[1]) {
+                divAd.classList.remove("fixed");
+                divAd.classList.remove("top-0");
+                divAd.classList.add("absolute");
+                divAd.classList.add("bottom-0");
+            }else {
+                divAd.classList.remove("fixed");
+                divAd.classList.add("sticky");
+                divAd.classList.remove("top-0");
+            }
+
+        }
+    }
+
+    useEffect(()=>{
+        window.addEventListener("scroll", handleContainerAdOnViewport);
+
+        return ()=> window.removeEventListener("scroll", handleContainerAdOnViewport)
+    },[])
+
+
     return (
         recipe &&
         <div className="w-full flex flex-col" key={recipe.id}>
@@ -72,12 +112,19 @@ export default function InfoRecipeContent({ recipe, user }) {
                 </div>
 
                 <div className="hidden md:block md:w-1/3 p-4">
-                    <LineAds display={true}>
-                        <div
-                            className="adsbyalkware"
-                            data-format="alk-display"
-                        ></div>
-                    </LineAds>
+                    <div 
+                        className="w-full h-[80vh] relative"
+                        ref={refContainerAd}
+                    >
+                        <div id="container-ads" className="sticky">
+                            <LineAds display={true}>
+                                <div
+                                    className="adsbyalkware"
+                                    data-format="alk-display"
+                                ></div>
+                            </LineAds>
+                        </div>
+                    </div>
                 </div>
 
             </div>
