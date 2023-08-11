913ef8b365790a458d19b5b71bb67f27bf5c536b
Guilherme Phellippe <guilhermephellippe@outlook.com>
2023-08-11 19:31:25 -0300

    improving the loading of page

--- src/components/atoms/Loading/LoadingInput.jsx
+++ src/components/atoms/Loading/LoadingInput.jsx
@@ -1,19 +1,12 @@
 import { useEffect, useRef } from "react";
-import icon from "../../../images/icon.svg";
 
 
 export default function LoadingInput() {
-    const refIcon = useRef();
     const refText = useRef();
   
     useEffect(()=>{
       const interval = setInterval(() => {
-        if(refIcon?.current && refText?.current) {
-          const opacity = refIcon.current.dataset.opacity === "1" ? ".8": "1";
-          refIcon.current.style.opacity = opacity;
-          refIcon.current.dataset.opacity = opacity;
-          refText.current.textContent = addingPoint(refText.current.textContent)
-        }
+        if(refText?.current) refText.current.textContent = addingPoint(refText.current.textContent)
       }, 500);
   
       return () => clearInterval(interval)
@@ -35,13 +28,6 @@ export default function LoadingInput() {
   
     return (
       <div className='w-screen h-screen flex flex-col justify-center items-center bg-white'>
-        <img 
-          ref={refIcon} 
-          className="w-1/2 md:w-1/12 transition-opacity duration-500" 
-          src={icon} 
-          alt={"Icone da Tem sabor"} 
-          data-opacity="1"
-        />
         <span ref={refText} className='text-s2 text-color_orange mt-4 font-semibold'>Carregando.</span>
       </div>
     )
