82436c37e184e97ba3e29e639c9623653404bb06
Guilherme Phellippe <guilhermephellippe@outlook.com>
2023-08-28 16:26:43 -0300

    fix a error redirect link

--- src/pages/Whatsapp/index.jsx
+++ src/pages/Whatsapp/index.jsx
@@ -1,4 +1,4 @@
-import { useContext, useEffect } from "react"
+import { useContext } from "react"
 
 import { UserContext } from '../../contexts/userProvider';
 import { HomeProvider } from '../../contexts/Home/HomeProvider';
