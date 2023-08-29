f54cc42f3e423fb5aa15c8daffcd37b8eebd9da2
Guilherme Phellippe <guilhermephellippe@outlook.com>
2023-08-29 13:09:22 -0300

    add manually ad

--- src/components/atoms/LineAds/ArticleAds.jsx
+++ src/components/atoms/LineAds/ArticleAds.jsx
@@ -6,9 +6,9 @@ const ArticleAds = () => {
             <div className="w-1/5">
                 <Img imgs={"https://m.media-amazon.com/images/I/41qN01i9TuL._AC_.jpg"} />
             </div>
-            <div className="flex flex-col">
-                <h2 className='text-s1_3 font-bold text-center'>Descobri o segredo das batatas de supermercado!!!</h2>
-                <div className="flex p-2 w-full h-1/2 items-end justify-between">
+            <div className="w-4/5 flex flex-col">
+                <h2 className='text-s1_3 h-1/2 p-4 font-bold text-center'>Descobri o segredo das batatas de supermercado!!!</h2>
+                <div className="flex p-2 md:px-4 w-full h-1/2 items-end justify-between">
                     <p className='text-s1_3 text-center w-3/4'>é esse produtinho aqui...</p>
                     <a
                         href="https://amzn.to/3YSHUDe"
